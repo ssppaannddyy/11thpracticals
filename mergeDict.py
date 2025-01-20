bc70a8ef1604e71e3ae9ef30c5138a16268950ec
ssppaannddyy <166301222+ssppaannddyy@users.noreply.github.com>
2025-01-20 18:27:04 +0530

    Add files via upload
    another big error

--- mergeDict.py
+++ mergeDict.py
@@ -3,22 +3,24 @@ dict1 = {}
 dict2 = {}
 print("Enter elements for the first dictionary")
 for i in range (0,n1):
-    a = input("Enter key: ")
-    if a in dict1.keys():
-        print("please try again, as the key already exists")
-        i=i-1
-        continue
-    b = input("Enter value corresponding to the key: ")
-    dict1.update({a:b})
+    while True:
+        a = input("Enter key: ")
+        if a in dict1.keys():
+            print("please try again, as the key already exists")
+            continue
+        b = input("Enter value corresponding to the key: ")
+        dict1.update({a:b})
+        break
 n2 = int(input("Enter number of elements to be in second dictionary: "))
 for i in range (0,n2):
-    c = input("Enter key: ")
-    if c in dict2.keys():
-        print("please try again, as the key already exists")
-        i=i-1
-        continue
-    d = input("Enter value corresponding to key: ")
-    dict2.update({c:d})
+    while True:
+        c = input("Enter key: ")
+        if c in dict2.keys():
+            print("please try again, as the key already exists")
+            continue
+        d = input("Enter value corresponding to key: ")
+        dict2.update({c:d})
+        break
 mDict = dict1.copy()
 for i in dict2.keys():
      if i not in dict1.keys():
