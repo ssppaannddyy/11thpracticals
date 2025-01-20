6e2ca7887f22f19776798fbe0d6a66d95dd394e3
ssppaannddyy <166301222+ssppaannddyy@users.noreply.github.com>
2025-01-20 17:47:39 +0530

    Add files via upload
    more errors

--- listsList.py
+++ listsList.py
@@ -2,11 +2,13 @@ n1 = int(input("Enter number of elements to be in the original list: "))
 print("Enter elements to be in the list")
 l1 = []
 for i in range (0,n1):
-    l1.append(input())
+    l1.append(int(input()))
 n2 = int(input("Enter number of elements to be put in second list: "))
 l2 = []
 for i in range (0,n2):
-    l2.append(input())
+    l2.append(int(input()))
+print("Original list: ",l1)
+print("Second list: ",l2)
 for i in l2:
     l1.append(i)
 print("Updated list after appending:",l1)
